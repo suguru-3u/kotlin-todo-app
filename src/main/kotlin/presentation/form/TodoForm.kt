13ef0025381a646d60b0fc641e0747d458c40812
suguru-3u <suguru.13579515@gmail.com>
2024-08-19 11:13:39 +0900

    commit時の警告の対応

--- src/main/kotlin/presentation/form/TodoForm.kt
+++ src/main/kotlin/presentation/form/TodoForm.kt
@@ -1,7 +1,5 @@
 package org.example.presentation.form
 
-import com.mysql.cj.xdevapi.Schema.Validation
-
 data class TodoForm(val title: String, var valResult: Boolean = title.isBlank()) {
     init {
         println("\n入力した内容: $title\n")
